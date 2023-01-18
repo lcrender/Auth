e3b43aeeffcb552502e07dd405e89dc27b9ab251
Alejandro Chazarreta <lcrender@gmail.com>
2023-01-18 12:52:45 +0100

    update

--- JWT/app/db.js
+++ JWT/app/db.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 const { DATABASE_HOST, DATABASE_USER, DATABASE_PASSWORD, DATABASE_NAME } = process.env;
-const MONGODB_URI = `mongodb+srv://${DATABASE_USER}:${DATABASE_PASSWORD}${DATABASE_HOST}${DATABASE_NAME}`;
+const MONGODB_URI = `mongodb+srv://${DATABASE_USER}:${DATABASE_PASSWORD}@${DATABASE_HOST}/${DATABASE_NAME}`;
 
 mongoose.set('strictQuery', false);
 mongoose.connect(MONGODB_URI, {
